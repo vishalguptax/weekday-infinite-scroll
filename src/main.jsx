0ada482c39e3569545fc8cda731e22a54bf4e9a4
Vishal Gupta <63388692+vishalguptax@users.noreply.github.com>
2024-05-05 14:03:36 +0530

    fix: page title and some clenups

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "./store";
-import { ThemeProvider } from "@mui/material";
 import { muiTheme } from "./config/muiTheme.js";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
